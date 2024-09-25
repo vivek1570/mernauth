cea78e8c0087033ee06c5e82a3bd8399f99a4ac7
vivek1570 <kpvivekvettam@gmail.com>
2024-09-25 14:18:19 +0530

    setting up pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { BrowserRouter,Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
 
 export default function App() {
-  return (
-    <h1 className='text-red-500'>
-      hello world
-    </h1>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+
+    </Routes>
+  </BrowserRouter>
 }
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <div>
+      <h1 className='text-red-400'>SIgn up herdjdjdehjfbkwfkweh</h1>
+    </div>
+  )
+}
